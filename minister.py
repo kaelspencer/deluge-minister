ed860c9c02a0399138cf58cdf6d8e2c7fca48163
kaelspencer <kaelspencer@gmail.com>
2014-06-30 19:57:09 -0700

    Only Add Directories at Target Depth
    All files encountered should be added to a list but only directories
    at the target level should be considered for action.

--- minister.py
+++ minister.py
@@ -17,11 +17,16 @@ def iterate_input(path, depth):
     for dir in os.listdir(path):
         if not os.path.islink(dir):
             absolute_path = '%s/%s' % (path, dir)
-            result.append((absolute_path, os.path.isdir(absolute_path)))
+            if should_be_included(absolute_path, depth == 0):
+                result.append((absolute_path, os.path.isdir(absolute_path)))
 
             if depth > 0 and os.path.isdir(absolute_path):
                 result.extend(iterate_input(absolute_path, depth - 1))
     return result
+
+
+def should_be_included(path, at_depth):
+    return at_depth or not os.path.isdir(path)
 
 if __name__ == '__main__':
     minister()
