673b467d064e66b554778fd5a525499001755f31
kaelspencer <kaelspencer@gmail.com>
2014-07-04 19:18:49 -0700

    Load Saved Processed List File
    Load the saved file. Used the imported data to exclude items that
    have alread been processed.

--- minister.py
+++ minister.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 import click
 import json
+from pprint import pprint
 import os
 
 
@@ -14,34 +15,48 @@ import os
               help='The file location to store processed files so duplication '
               'doesn\'t happen in the future.')
 def minister(target, depth, storage_file):
-    targets = iterate_input(target, depth)
-    save_output(storage_file, [x[0] for x in targets])
+    already_processed = load_storage_file(storage_file)
+    targets = iterate_input(target, depth, already_processed)
+    pprint(targets)
+    save_storage_fle(storage_file, [x[0] for x in targets] + already_processed)
 
 
-def iterate_input(path, depth):
+def iterate_input(path, depth, already_processed):
     result = []
 
     for dir in os.listdir(path):
         if not os.path.islink(dir):
-            absolute_path = '%s/%s' % (path, dir)
-            if should_be_included(absolute_path, depth == 0):
-                result.append((absolute_path, os.path.isdir(absolute_path)))
+            abs_path = '%s/%s' % (path, dir)
+            if should_be_included(abs_path, depth == 0, already_processed):
+                result.append((abs_path, os.path.isdir(abs_path)))
 
-            if depth > 0 and os.path.isdir(absolute_path):
-                result.extend(iterate_input(absolute_path, depth - 1))
+            if depth > 0 and os.path.isdir(abs_path):
+                result.extend(iterate_input(abs_path, depth - 1))
     return result
 
 
-def should_be_included(path, at_depth):
-    return at_depth or not os.path.isdir(path)
+def should_be_included(path, at_depth, already_processed):
+    valid = at_depth or not os.path.isdir(path)
+    return valid and path not in already_processed
 
 
-def save_output(file, processed):
+def save_storage_fle(file, processed):
     f = open(file, 'w')
     f.write(json.dumps(processed, sort_keys=True, indent=4,
                        separators=(',', ': ')))
     f.write('\n')
     f.close()
+
+
+def load_storage_file(file):
+    try:
+        f = open(file, 'r')
+        lines = f.readlines()
+        lines = json.loads(''.join(lines))
+        f.close()
+        return lines
+    except:
+        return []
 
 if __name__ == '__main__':
     minister()
