f8364b2a9a24794aeb76f95940a7f0416ea7da7b
kaelspencer <kaelspencer@gmail.com>
2017-09-27 13:59:32 -0700

    Cleanup poll example

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,23 @@
+#include <errno.h>
+#include <fcntl.h>
+#include <poll.h>
 #include <stdio.h>
-#include <sys/types.h>
+#include <stdlib.h>
 #include <sys/stat.h>
 #include <unistd.h>
-#include <fcntl.h>
-#include <stdlib.h>
-#include <errno.h>
-#include <aio.h>
-#include <poll.h>
-
-void async_read(sigval val)
-{
-    puts("callback");
-    struct aiocb *ptr = (struct aiocb *)val.sival_ptr;
-    printf("read=%s", (char *)ptr->aio_buf);
-}
 
 void read_fd(int fd)
 {
-    char buf[100] = {};
-    (void)read(fd, buf, sizeof(buf));
+    char buf[1024] = {};
+    int result = read(fd, buf, sizeof(buf));
+
+    if (result == -1 && errno == EAGAIN)
+    {
+        // We need to go back to poll().
+        puts("Received EAGAIN from read");
+        return;
+    }
+
     puts(buf);
 }
 
@@ -55,55 +54,6 @@ int main()
             exit(1);
         }
     }
-
-    // while (true)
-    // {
-    //     char buf[1024] = {};
-    //     int result = read(fd, buf, sizeof(buf));
-
-    //     while (result <= 0)
-    //     {
-    //         puts("sleeping...");
-    //         sleep(1);
-    //         result = read(fd, buf, sizeof(buf));
-    //     }
-
-    //     puts(buf);
-    // }
-
-
-
-    // struct aiocb cb = {};
-    // char sbuf[100];
-    // int ret;
-
-    // // bzero(&cb, sizeof(cb));
-    // // memset(&cb, sizeof(cb));
-
-    // cb.aio_fildes = fd;
-    // cb.aio_buf = sbuf;
-    // cb.aio_nbytes = 100;
-    // cb.aio_offset = 0;
-
-    // cb.aio_sigevent.sigev_value.sival_ptr = &cb;
-    // cb.aio_sigevent.sigev_notify = SIGEV_THREAD;
-    // cb.aio_sigevent.sigev_notify_function = async_read;
-    // cb.aio_sigevent.sigev_notify_attributes = NULL;
-    // cb.aio_sigevent.sigev_signo = SIGUSR1;
-
-    // ret = aio_read(&cb);
-
-    // if (ret == -1) {
-    //     perror("aio_read");
-    //     exit(1);
-    // }
-
-    // puts("sleeping");
-
-    // while (1) {
-    //     sleep(3);
-    // }
-
 
     return 0;
 }
